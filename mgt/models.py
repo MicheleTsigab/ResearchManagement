9ed975f6c333a58384d5974e120e0b15be7b5e72
Michele <micheletsigab@gmail.com>
2022-03-11 15:41:58 +0300

    staff model complete

--- mgt/models.py
+++ mgt/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+class Staff(models.Model):
+    id=models.AutoField(primary_key=True)
+    first_name=models.CharField(max_length=150)
+    last_name=models.CharField(max_length=150)
+    phone=models.CharField(max_length=30,default="09000000",null=True)
+    password=models.CharField(max_length=128)
+    email=models.CharField(max_length=254)
+    last_login=models.DateField()
+    is_active=models.BooleanField()
+    date_joined=models.DateField()
+    class Meta:
+        db_table = "staff"
 
